6952381dac6583650ef09eabee2629756653dc40
Jp1punto5 <juan_berrios1996@hotmail.com>
2024-04-28 18:26:38 -0400

    new file:   CSS/estilo_menu.css 	renamed:    my_estilo_home.css -> CSS/my_estilo_home.css 	new file:   SCRIPT/principal.js 	modified:   home.html 	new file:   menu.html

---
+++ SCRIPT/principal.js
@@ -0,0 +1,53 @@
+
+// AQUI COMIENZAN LAS FUNCIONES // 
+
+var admin = "jp1punto5"; // variable global
+var contra = "123456";
+
+function validar(boton_enviar)
+
+{
+
+    let user = document.getElementById("user").value;
+    let pass = document.getElementById("pass").value;
+
+    let validar = 0;
+    if(user == null || !user.trim())
+    {
+        alert("Ingrese el Nombre Usuario sin espacios vacios..");
+        validar = 1;
+    }
+
+    if(pass == null || !pass.trim())
+    {
+        alert("ingrese su clave sin espacios...")    
+        validar = 1;
+    }
+
+    if (validar == 0) 
+    {
+        if (user!== admin) {
+            alert("ingrese un usuario existente..");
+            validar = 2;
+        }
+    
+        if (pass !== contra) {
+            alert("la clave ingresada es incorrecta..");
+            validar = 2;
+        }
+
+        if (validar == 2) 
+        {
+            return false;    
+        }else 
+        {
+            return boton_enviar;
+        }
+
+    }else
+    {
+        return false;
+    }
+
+}// fin funcion de validar campos y enviar el boton devuelta al formulario
+
